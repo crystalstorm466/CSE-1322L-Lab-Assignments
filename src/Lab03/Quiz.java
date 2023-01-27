e19e845c8a6a186107fe1bb31266cefe698dd78b
David Holland <dcholland_89@msn.com>
2023-01-27 15:53:54 +0000

    Here

--- src/Lab03/Quiz.java
+++ src/Lab03/Quiz.java
@@ -7,16 +7,13 @@ public class Quiz {
     private Scanner scan = new Scanner(System.in);
     public void add_question() {
         System.out.print("What is the question? ");
-        scan.nextLine();
         String ques = scan.nextLine();
-        System.out.print("What is the answer? ");
         scan.nextLine();
+        System.out.print("What is the answer? ");
         String ans = scan.nextLine();
-        ans.toLowerCase();
         System.out.print("What is the difficulty? ");
         int diff = scan.nextInt();
         Question newQuestion = new Question(ques, ans, diff);
-        
         QuestionsList.add(newQuestion);
     }
 
@@ -37,33 +34,27 @@ public class Quiz {
         int userInput = scan.nextInt();
 
         System.out.print("What is the new question? ");
-        String newQues = scan.nextLine();
-        scan.nextLine();
-        QuestionsList.get(userInput).setQuestionText(newQues);
+        QuestionsList.get(userInput).setQuestionText(scan.nextLine());
         System.out.print("What is the new answer? ");
-        String newAns = scan.nextLine();
-        QuestionsList.get(userInput).setQuestionAnswer(newAns);
+        QuestionsList.get(userInput).setQuestionAnswer(scan.nextLine());
         System.out.print("What is the new difficulty? ");
-        int diff = scan.nextInt();
-        QuestionsList.get(userInput).setDifficulty(diff);
+        QuestionsList.get(userInput).setDifficulty(scan.nextInt());
     }
 
     public void give_quiz() {
         int correct_Answers = 0;
         int quizSize = 0;
         for (int i = 0; i < QuestionsList.size(); i++) {
+            System.out.println(i + ": " + QuestionsList.get(i).getQuestionText());
             quizSize++;
-            System.out.println(i+1 + ": " + QuestionsList.get(i).getQuestionText());
-            scan.nextLine();
             String answer = scan.nextLine();
-            answer.toLowerCase();
-            if (answer.equals(QuestionsList.get(i).getQuestionAnswer()))  {
+            scan.nextLine();
+            if (answer.equalsIgnoreCase(QuestionsList.get(i).getQuestionAnswer()))  {
                 System.out.println("Correct!");
                 correct_Answers++;
-            } else {
-                System.out.println("Incorrect");
             }
         }
-        System.out.println("You got " + correct_Answers + " correct out of " + quizSize);
+        System.out.println(correct_Answers / quizSize + " out of " + quizSize + " correct.");
+
     }
 }
