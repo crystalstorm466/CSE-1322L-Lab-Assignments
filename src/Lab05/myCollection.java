df26b4c5660c299345fe947a3818283b7031cb9d
David Holland <DcHolland_89@msn.com>
2023-02-01 13:04:11 -0500

    works and figured out how to take multiple words in input finally put scan.nextline above  the print statement that asks for your input

--- src/Lab05/myCollection.java
+++ src/Lab05/myCollection.java
@@ -7,31 +7,31 @@ public class myCollection {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayList<Item> Library = new ArrayList<>();
-        for (int i = 0; i <= 5; i++) {
-            System.out.print("Please enter B for Book or P for Periodical");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Please enter B for Book or P for Periodical: ");
             String userChar = scan.next();
             if (userChar.equalsIgnoreCase("B")) {
-                System.out.print("What is the title? ");
+                scan.nextLine();
+                System.out.print("What is the title?: ");
                 String title = scan.nextLine();
-                scan.next();
-                System.out.print("What is the author? ");
+                scan.nextLine();
+                System.out.println("What is the author?: ");
                 String author = scan.nextLine();
-                scan.next();
+                scan.nextLine();
                 System.out.print("What is the ISBN Number? ");
                 int isbn = scan.nextInt();
                 Book newBook = new Book(isbn, author, title);
                 Library.add(newBook);
-            } else if (userChar.equalsIgnoreCase("C")) {
+            } else if (userChar.equalsIgnoreCase("P")) {
+                scan.nextLine();
                 System.out.print("What is the title? ");
                 String tile = scan.nextLine();
-                scan.next();
                 System.out.print("What is the issue number? ");
                 int issue = scan.nextInt();
                 Periodical newPeriod = new Periodical(tile, issue);
                 Library.add(newPeriod);
             }
         }
-
         for (Item item : Library) {
             System.out.println(item.getListing());
         }
