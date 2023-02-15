1b4295b204543f3078ec9cf252d9cd9a3f49d161
David Holland <DcHolland_89@msn.com>
2023-02-15 13:05:26 -0500

    Lab 6 works 100%.

--- src/Lab06/FindIteration.java
+++ src/Lab06/FindIteration.java
@@ -4,14 +4,16 @@ public class FindIteration implements FindFib {
 
     @Override
     public int calculate_fib(int n) {
-        if (n == 1 || n == 2) {
+       if (n == 1 || n == 2) {
             return 1;
         }
-        int previous = 1;
+        int temp;
+        int previous = 0;
         int fib = 1;
-        for (int i = 0; i <= n; i++) {
-         int temp = fib;
-         fib = fib + previous;
+        for (int i = 1; i < n; i++) {
+         temp = previous;
+         previous = fib;
+         fib = temp + previous;
         // previous = temp;
 
             //  answer =  ((answer - i) + (answer - (i+ 1)));
