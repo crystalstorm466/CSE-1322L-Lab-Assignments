15e5e526d1604fafa0d839d659f4435c2e9ddf5a
David Holland <dcholland_89@msn.com>
2023-03-27 14:44:16 +0000

    redoing 6 because its easier

--- src/Assignment06/Assignment6.java
+++ src/Assignment06/Assignment6.java
@@ -1,91 +1,34 @@
 package Assignment06;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Assignment6 {
+public class Assignment6Driver {
+    public static ArrayList<String> getData() throws UnknownHostException, IOException {
+        ArrayList<String> sockeList = new ArrayList<>();
 
-    public static ArrayList<String> getData () throws IOException {
-        ArrayList<String> socketList = new ArrayList<String>();
         Socket socket = new Socket("api.coindesk.com", 80);
         OutputStream output = socket.getOutputStream();
         PrintWriter writer = new PrintWriter(output, true);
-        writer.write("GET http://api.coindesk.com/v1/bpi/currentprice.json HTTP/1.0");
-        output.flush();
-        Scanner socketscanner = new Scanner(socket.getInputStream());
-        
-        while (socketscanner.hasNextLine()) {
-            String result = socketscanner.nextLine();
-            socketList.add(result);
+        writer.write("GET http://api.coindesk.com/v1/bpi/currentprice.json HTTP/1.0\n\n");
+        Scanner socketScanner = new Scanner(socket.getInputStream());
+        while (socketScanner.hasNextLine()) {
+            sockeList.add(socketScanner.nextLine());
         }
         writer.close();
-        socket.close();
-        socketscanner.close();
-
-        return socketList;
-    }
-
-    //Java students:  Copy and paste this method into your main driver class:
-
-    public static float getDollarPrice(ArrayList<String> lines) {
-        boolean header=true;
-        String json="";
-        for(String line : lines) {
-            if(line.equals("")) {
-                header=false;
-                continue;
-            }
-            if(header==false) {
-                json=line;
-                break;
-            }
-        }
-        //System.out.println("Json: "+json);
-        String[] jsonParts=json.split(":");
-        String priceLine=jsonParts[19];
-        String justPrice=priceLine.replace("},\"GBP\"","");
-        float price=Float.parseFloat(justPrice);
-        return price;
-    }
-
-    public static void buyBitcoin(float bitcoinPrice) {
-        PrintWriter pw = null;
+        socketScanner.close();
         try {
-            File investmentUSD = new File("initialInvestmentUSD.txt");
-            Scanner read = new Scanner(investmentUSD);
-            ArrayList<String> investmentArray = new ArrayList<String>();
-            int linecount = 0;
-            while (read.hasNextLine()) {
-                linecount++;
-
-                String[] parts = read.nextLine().split(":");
-                String name = parts[linecount].trim();
-                investmentArray.add(name);
-                String investment = parts[linecount+1].trim();
-                investmentArray.add(investment);
-                // investmentArray.get(linecount).split(":");
-                // investmentArray.add(read.nextLine());
-
-            }
-            float dollars = getDollarPrice(investmentArray);
-            File bitcointrans = new File("clientBC.txt");
-            pw = new PrintWriter(bitcointrans);
-            read = new Scanner(investmentUSD);
-            linecount = 0;
-            while (read.hasNextLine()) {
-                linecount++;
-                pw.println(parts[linecount] + ":" + parts[linecount+1]);
-            }
-    } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            socket.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-
+        return sockeList;
+         
     }
-
-    private static float getCurrentValue(float bitcoin) {
-        return 0;
-    }
-    }
+}
