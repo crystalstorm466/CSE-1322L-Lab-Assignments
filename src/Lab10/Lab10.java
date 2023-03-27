a5dc590c0a201d22c3058f0cfd3d2dcdc29a41d2
David Holland <dcholland_89@msn.com>
2023-03-27 14:02:53 +0000

    Updated Lab10

--- src/Lab10/Lab10.java
+++ src/Lab10/Lab10.java
@@ -2,36 +2,51 @@ package Lab10;
 
 import java.io.*;
 import java.nio.file.FileStore;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lab10 {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         System.out.print("What is the first filename? ");
-        String firstFile = scan.next(); //do compare1.txt
+        String firstFile = scan.next(); //do Lab10A.txt
         System.out.print("What is the second filename? ");
-        String secondFile = scan.next(); //do compare2.txt
+        String secondFile = scan.next(); //do Lab10B.txt
         String previous = null;
+        ArrayList<String> fileList = new ArrayList<>();
+        ArrayList<String> secondFileList = new ArrayList<>();
         int line = 0;
+        int secondLine = 0;
         try {
 
             Scanner firstScanner = new Scanner(new File(firstFile));
             Scanner secondScanner = new Scanner(new File(secondFile));
             while (firstScanner.hasNextLine()) {
-                previous = scan.nextLine();
+                fileList.add(firstScanner.nextLine());
                 line++;
             }
-
-
             while (secondScanner.hasNextLine()) {
-                if (!(previous.equals(secondScanner.nextLine()))) {
-                    FileOutputStream fos = new FileOutputStream(secondFile, true);
-                    PrintWriter pw = new PrintWriter(fos);
-                    pw.println("Line" + line + "\n< " + previous);
-                    pw.println("> " + secondScanner.nextLine());
+                secondFileList.add(secondScanner.nextLine());
+                secondLine++;
+            }
+            if (!(line == secondLine)) {
+                System.out.println("Files have different number of lines");
+                firstScanner.close();
+                secondScanner.close();
+                return; //exits the program
+            }
+            int i = 0;
+            for ( String s : fileList ) {
+                i++;
+                if (fileList.get(i) != secondFileList.get(i)) {
+                    System.out.println("Line: " + i);
+                    System.out.println("< " +secondFileList.get(i));
+                    System.out.println("> "+fileList.get(i));
                 }
             }
+
             firstScanner.close();
+            secondScanner.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
